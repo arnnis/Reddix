7a71759290f179774b500e8a0abda2f31152b18c
Alireza Rezania <alireza.rzna@gmail.com>
2020-08-06 21:25:55 +0430

    another fix for react-router in ghpages

--- src/navigation/paths.ts
+++ src/navigation/paths.ts
@@ -1,6 +1,8 @@
-export const HOME_AND_SUBREDDIT_PATH = "/(r?)/:subreddit?";
-export const HOME_PATH = "/";
-export const SUBREDDIT_PATH = "/r/:subreddit";
-export const POST_PATH = "/r/:subreddit/comments/:postId";
-export const SAVED_PATH = "/saved";
-export const SETTINGS_PATH = "/settings";
+export const HOME_AND_SUBREDDIT_PATH =
+  process.env.PUBLIC_URL + "/(r?)/:subreddit?";
+export const HOME_PATH = process.env.PUBLIC_URL + "/";
+export const SUBREDDIT_PATH = process.env.PUBLIC_URL + "/r/:subreddit";
+export const POST_PATH =
+  process.env.PUBLIC_URL + "/r/:subreddit/comments/:postId";
+export const SAVED_PATH = process.env.PUBLIC_URL + "/saved";
+export const SETTINGS_PATH = process.env.PUBLIC_URL + "/settings";
