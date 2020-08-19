f1c75d17fcae2348aee791ff3e3ccec3b1313fe9
Alireza Rezania <alireza.rzna@gmail.com>
2020-08-19 17:00:49 +0430

    handle oauth in dev & prod environments

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,6 @@
+// general
 export const PUBLIC_API_URL = "https://www.reddit.com";
 export const OAUTH_API_URL = "https://oauth.reddit.com";
-export const APP_URL = "http://localhost:3000/Reddix";
-export const CLIENT_ID = "-q-aaV4uoRgELg";
 const SCOPES = [
   "identity",
   "account",
@@ -13,9 +12,21 @@ const SCOPES = [
   "save",
   "vote",
 ];
+export const DEFAULT_TITLE = "Reddix for reddit";
+
+// prod
+export const APP_URL_PROD = "https://arrnis.github.io/Reddix";
+export const CLIENT_ID_PROD = "-q-aaV4uoRgELg";
+
+// Dev
+const ID_DEV = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+const CLIENT_ID_DEV = "7IWjDq1H8IAtXA";
+export const APP_URL_DEV = "http://localhost:3000/Reddix";
+
+export const APP_URL = ID_DEV ? APP_URL_DEV : APP_URL_PROD;
+export const CLIENT_ID = ID_DEV ? CLIENT_ID_DEV : CLIENT_ID_PROD;
+
 export const OAUTH_CALLBACK_URL = `${APP_URL}/oauthcallback`;
 export const OAUTH_URL = `https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=code&state=9043316759&redirect_uri=${OAUTH_CALLBACK_URL}&duration=permanent&scope=${SCOPES.join(
   " "
 )}`;
-
-export const DEFAULT_TITLE = "Reddix for reddit";
