a27c789a2689989159b5669c15213291b14cdb24
Alireza Rezania <alireza.rzna@gmail.com>
2020-08-22 19:25:16 +0430

    minor fix

--- src/pages/home/posts-list.tsx
+++ src/pages/home/posts-list.tsx
@@ -33,6 +33,7 @@ const PostList: FC = () => {
   const prevCategory = usePrevious(category);
 
   useEffect(() => {
+    if (matchHome) return;
     if (
       matchSubreddit?.params.subreddit !==
         // @ts-ignore
