5fe02292856f468d0220532d5556b4223532c642
arnnis <arnnis@users.noreply.github.com>
2020-08-22 16:49:07 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.c0563a9285adf289ab27ce18c9bbdac8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034b3bacf9db589b1d2bfaebf5077d96",
+    "url": "/Reddix/index.html"
+  },
+  {
+    "revision": "2029e094c9cf530c3fda",
+    "url": "/Reddix/static/css/main.4c31d33a.chunk.css"
+  },
+  {
+    "revision": "a16af7c3b3b3d9d20ab8",
+    "url": "/Reddix/static/js/2.5b5b500c.chunk.js"
+  },
+  {
+    "revision": "613f25066f338e606488ffc444858e67",
+    "url": "/Reddix/static/js/2.5b5b500c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2029e094c9cf530c3fda",
+    "url": "/Reddix/static/js/main.1d3f40f2.chunk.js"
+  },
+  {
+    "revision": "a8e0e334645f9c7572ae",
+    "url": "/Reddix/static/js/runtime-main.6abbed97.js"
+  },
+  {
+    "revision": "1c66cb05d2988c15d88a2d711a716f04",
+    "url": "/Reddix/static/media/arrow-down-bold.1c66cb05.svg"
+  },
+  {
+    "revision": "0fbf8c9a7de1ecdaa11eceb74dc05f17",
+    "url": "/Reddix/static/media/arrow-up-bold.0fbf8c9a.svg"
+  },
+  {
+    "revision": "11bdd8ee6a5598fe93f72f6391b1fa46",
+    "url": "/Reddix/static/media/bookmark-plus.11bdd8ee.svg"
+  },
+  {
+    "revision": "cfcc835772b91de4320f6bd4f435abc6",
+    "url": "/Reddix/static/media/chevron-down.cfcc8357.svg"
+  },
+  {
+    "revision": "857fd861dcb26b7405e7506bc01987be",
+    "url": "/Reddix/static/media/chevron-up.857fd861.svg"
+  },
+  {
+    "revision": "fea2ee662c1f8d8d66794c65cc7733ed",
+    "url": "/Reddix/static/media/cog-box.fea2ee66.svg"
+  },
+  {
+    "revision": "8ecb1681ad5cd712817fce531504a597",
+    "url": "/Reddix/static/media/comment.8ecb1681.svg"
+  },
+  {
+    "revision": "940ed8fd3fd3ba065e3ec50f685a32ec",
+    "url": "/Reddix/static/media/content-save.940ed8fd.svg"
+  },
+  {
+    "revision": "0c07127cfd4828956e73108b32efcd35",
+    "url": "/Reddix/static/media/home.0c07127c.svg"
+  },
+  {
+    "revision": "b91bdf1b3fc2124f338895fdb6b121be",
+    "url": "/Reddix/static/media/plus.b91bdf1b.svg"
+  },
+  {
+    "revision": "be60bd2065c44d8c5cf089424846bc3e",
+    "url": "/Reddix/static/media/theme-light-dark.be60bd20.svg"
+  }
+]);
